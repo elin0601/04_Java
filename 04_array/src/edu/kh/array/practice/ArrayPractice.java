78fffec83420db37c5a44ae95c3a679dd6a2b613
dlwltn <03dreams@naver.com>
2024-02-01 12:47:20 +0900

    240201_배열 + 2차원 배열

--- 04_array/src/edu/kh/array/practice/ArrayPractice.java
+++ 04_array/src/edu/kh/array/practice/ArrayPractice.java
@@ -1,5 +1,6 @@
 package edu.kh.array.practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayPractice {
@@ -166,4 +167,175 @@ public class ArrayPractice {
 		}
 
 	}
+	
+	
+	public void practice7() {
+		
+		// while 문으로 조건에 맞는 수가 입력 될 때 까지 반복
+		// + break
+		
+		// 배열 요소에 대입되는 숫자는 count++, -- 를 이용해 제어
+		
+		int len = 0;
+
+		while (true) { // 3 이상의 홀수가 입력될 때 까지 무한 반복
+			System.out.print("정수 : ");
+			len = sc.nextInt();
+			
+			if(len % 2 == 1 && len >=3)  {
+				break;
+			}
+			
+			// if문 수행 X == 3 이상이 아니거나 짝수인 경우
+			System.out.println("다시 입력하세요");
+		}
+		// 입력 받은 정수 크기 만큼의 배열 선언 + 할당
+		int[] arr = new int[len];
+		
+		int count = 0; // arr 배열 요소에 대입될 수
+		
+		for( int i = 0; i< len; i++) {
+			
+			 if (i<= len / 2) count ++;
+			 
+			 else  count--;
+			 
+			 arr[i] = count;
+		}
+		// Arrays.toString(배열명)
+		// => 배열 내 요소를 "[1,2,3]" 형식의 문자열로 만들어 반환
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	
+	public void practice8() {
+
+		int[] randoms = new int[10];
+
+		String str = "";
+
+		for (int i = 0; i < randoms.length; i++) {
+
+			randoms[i] = (int) (Math.random() * 10 + 1);
+
+			str += randoms[i]; // 누적
+		}
+
+		System.out.println("발생한 난수 :" + str);
+	}
+	
+	
+	public void practice9() {
+
+		int[] randoms = new int[10];
+
+		int min = 0;
+		int max = 0;
+
+		String str = "";
+
+		for (int i = 0; i < randoms.length; i++) {
+
+			randoms[i] = (int) (Math.random() * 10 + 1);
+
+			if (i == 0) { // 첫 반복인 경우 -> 첫 번째 난수를 비교 기준으로 삼음
+				max = randoms[i];
+				min = randoms[i];
+				continue; // 다음 반복으로 이동
+
+			}
+
+			// 첫 반복이 아닌 경우
+			if (randoms[i] > max)
+				max = randoms[i];
+			
+			if (randoms[i] < min)
+				min = randoms[i];
+
+			str += randoms[i]; // 누적
+		}
+
+		System.out.println("발생한 난수 :" + str);
+		System.out.println("\n최댓값 : " + max);
+		System.out.println("최솟값 : " + min);
+	}
+
+	
+	/**
+	 * 8번 문제에서 중복을 제외한 코드
+	 */
+	public void practice10() {
+
+		int[] randoms = new int[10];
+		String str = "";
+
+		// 난수 생성용 for문
+		for (int i = 0; i < randoms.length; i++) {
+			randoms[i] = (int) (Math.random() * 10 + 1);
+
+			// * 중복 검사 방법 *
+			// 현재 인덱스에 대입된 난수와 같은 값이
+			// 앞쪽 인덱스에 존재하면
+			// 다시 현재 인덱스에 새로운 난수를 생성 해서 대입
+			// (중복이 없을 때 까지 반복)
+
+			// 현제 인덱스 앞쪽까지만 순차 접근하는 for문
+			for (int x = 0; x < i; x++) {
+
+				// 현재 인덱스에 저장된 값과(지금 발생한 난수와)
+				// 앞쪽 인덱스에 저장된 값이(이전에 발생한 난수와)
+				// 같을 경우 "중복"으로 판단!!
+				if (randoms[i] == randoms[x]) {
+
+					i--; // 바깥쪽 for문의 i 값 증가를 막아서
+								// 현재 인덱스 번째 요소에 다시 새로운 난수를 대입하게 함
+					break; // 중복을 발견 했으니 추가적인 검사사 필요 없음
+				}
+			}
+		}
+
+		// 출력용 구문 만들기용 for문
+		for (int i = 0; i < randoms.length; i++) {
+			str += randoms[i] + " ";
+		}
+		System.out.println("발생한 난수 : " + str);
+	}
+//-> while문 이용해서 해보기
+
+	
+	public void practice11() {
+
+		int[] lotto = new int[6];
+
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i] = (int) (Math.random() * 45 + 1);
+
+			for (int x = 0; x < i; x++) {
+				if (lotto[i] == lotto[x]) {
+					i--;
+					break;
+				}
+			}			
+		}
+		
+		// 오름차순 정렬(선택 정렬)
+		for (int i = 0; i < lotto.length - 1; i++) { // 칸 지정(비교 주체)
+
+			for (int x = 1 + i; x < lotto.length; x++) { // 비교 대상 두 번째 칸부터 비교 시작
+				if (lotto[i] > lotto[x]) { // 주체가 대상보다 크면 교체(swap) // (주체 > 대상 이면 오름차순 / 주체 < 대상 이면 내림차순)
+
+					int temp = lotto[i];
+					lotto[i] = lotto[x];
+					lotto[x] = temp; // 자리가 바뀜
+				}
+			}
+		}
+
+		// Arrays.sort(lotto);   // 자바에서 만들어서 제공해주는 
+															// 배열 오른차순 정렬 기능
+		
+		System.out.println(Arrays.toString(lotto));
+	}
+	
+	
 }
--- 04_array/src/edu/kh/array/practice/PracticeRun.java
+++ 04_array/src/edu/kh/array/practice/PracticeRun.java
@@ -9,6 +9,13 @@ public class PracticeRun {
 		// ap.practice1();
 		// ap.practice2();
 		// ap.practice3();
-		ap.practice4();
+		// ap.practice4();
+		// ap.practice5();
+		// ap.practice6();
+		// ap.practice7();
+		// ap.practice8();
+		// ap.practice9();
+		// ap.practice10();
+		ap.practice11();
 	}
 }
