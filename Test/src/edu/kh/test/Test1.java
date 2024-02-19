d106f8f0c69230a11d4dbd225237198dc9b509d7
dlwltn <03dreams@naver.com>
2024-02-19 13:50:56 +0900

    240219_프로그래밍 언어 활용 시험(더 풀이 해보기)

---
+++ Test/src/edu/kh/test/Test1.java
@@ -0,0 +1,37 @@
+package edu.kh.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		List<IPInfo> ipInfoList = new ArrayList<IPInfo>();
+		
+		ipInfoList.add(new IPInfo ("123.123.123.123", "홍길동"));
+		ipInfoList.add(new IPInfo ("212.133.7.8",""));
+		ipInfoList.add(new IPInfo("177.233.111.222", "고길순"));
+		
+		System.out.print("ip 입력 : ");
+		String ip = sc.nextLine();
+		
+		for (int i= 0; i<ipInfoList.size(); i++ ){
+			if(ipInfoList.get(i).equals(ip)) {
+				
+			System.out.println(ipInfoList.get(i));
+			break;
+			}
+			
+		}
+		
+		System.out.println("일치하는 ip 사용자가 없습니다.");
+	}
+	
+}
+
+
